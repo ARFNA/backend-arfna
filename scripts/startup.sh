caf303754b634c9626b250371d0faf914e4642aa
Roshnee Sharma <37020881+roshnee99@users.noreply.github.com>
2022-12-13 16:27:19 -0500

    Setting up to ignore system out so job doesn’t sit open for too long

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -4,4 +4,4 @@ echo "About to start up!"
 current_dir=$(pwd)
 war_file=${current_dir}/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/livewar-assembly/target/livewar-example.war
 echo ${war_file}
-sudo java -jar ${war_file} --add-to-startd=jsp
+sudo java -jar ${war_file} --add-to-startd=jsp > /dev/null 2> /dev/null < /dev/null &
