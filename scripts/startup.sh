b49fbaf5cb99f4205e14b486f6a0eaefe9108ea4
Roshnee Sharma <37020881+roshnee99@users.noreply.github.com>
2022-12-13 15:44:05 -0500

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "About to start up!"
-java -jar livewar-assembly/target/livewar-example.war >> logs.txt
+java -jar livewar-assembly/target/livewar-example.war --add-to-startd=jsp
