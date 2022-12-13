95dca50feb18a62b91f700c04f542b80c7d077b5
Roshnee Sharma <37020881+roshnee99@users.noreply.github.com>
2022-12-13 16:13:28 -0500

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,6 +2,6 @@
 
 echo "About to start up!"
 current_dir=$(pwd)
-war_file=${current_dir}/livewar-assembly/target/livewar-example.war
-echo war_file
+war_file=${current_dir}/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/livewar-assembly/target/livewar-example.war
+echo ${war_file}
 sudo java -jar ${war_file} --add-to-startd=jsp
