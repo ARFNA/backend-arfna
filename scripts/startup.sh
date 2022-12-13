4e1f97c41b191befda462e95bedefba1057cab3f
Roshnee Sharma <37020881+roshnee99@users.noreply.github.com>
2022-12-13 17:16:51 -0500

    Redirecting system out to log file (#30)
    * Update startup.sh
    
    * Update appspec.yml

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,6 +2,7 @@
 
 echo "About to start up!"
 current_dir=$(pwd)
-war_file=${current_dir}/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/livewar-assembly/target/livewar-example.war
-echo ${war_file}
-sudo java -jar ${war_file} --add-to-startd=jsp > /dev/null 2> /dev/null < /dev/null &
+deployment_dir=${current_dir}/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
+war_file=${deployment_dir}/livewar-assembly/target/livewar-example.war
+log_file=${deployment_dir}/logs.txt
+java -jar ${war_file} --add-to-startd=jsp >> ${log_file} 2>&1 &
